c711b39431aa2dc2df1db094d6af14133c914260
Bocha <Adiel12430@gmail.com>
2019-08-18 17:16:41 +0300

    More logic Tweaking
    Added the minShieldPrecentage variable.

--- Slasher/Assets/Scripts/GameManager.cs
+++ Slasher/Assets/Scripts/GameManager.cs
@@ -37,7 +37,8 @@ public class GameManager : MonoBehaviour
 
     bool finishedThinking;
     public int difficulty;
-    float difficultyB = 1.02f, difficultyX;
+    float initialValue = 1.02f, initialValuePower;
+    float shieldSpawnMinPrecentage = 100;
     Vector3 spawnPosition = new Vector3(0, 1.25f, 50); // Its outside the method because it needs to keep the record of all the past spawns.
     void SpawnBrain()
     {
@@ -54,26 +55,26 @@ public class GameManager : MonoBehaviour
         {
             // Will need logic to check for type...
             //Logic for Type.
-            type = Random.Range(0f, 101) > 96 ? SpawnInformation.ObjectType.Shield : SpawnInformation.ObjectType.Regular;
+            type = Random.Range(0f, 101) > shieldSpawnMinPrecentage ? SpawnInformation.ObjectType.Shield : SpawnInformation.ObjectType.Regular;
             #region Regular
             if (type == SpawnInformation.ObjectType.Regular)
             {
-                int maxSegementLength = Random.Range(1, difficulty / 3); // Used by difficulty.
-                maxSegementLength = Mathf.Clamp(maxSegementLength, 1, 7);
-                int segemtnLength = Random.Range(1, maxSegementLength);
+                //int maxSegementLength = Random.Range(1, 1+ Mathf.RoundToInt(difficulty / 2f)); // Used by difficulty.
+                //maxSegementLength = Mathf.Clamp(maxSegementLength, 1, 7);
+                int segemtnLength = Random.Range(1, 7);
                 if (i + segemtnLength > maxSpawnedObj - 1)
                     segemtnLength = maxSpawnedObj - i;
 
                 bool randomize = Random.Range(0, 11) > 7 ? true : false;
 
                 // Y position needs to be changed between types!!
-                spawnPosition = randomize ? spawnPosition : new Vector3(Random.Range(1, 4) * 2 - 5, 1.25f, spawnPosition.z);
+                spawnPosition = randomize ? spawnPosition : new Vector3(Random.Range(1, 5) * 2 - 5, 1.25f, spawnPosition.z);
 
                 for (int tempI = 0; tempI < segemtnLength; tempI++)
                 {
                     if (randomize)
                     {
-                        spawnPosition = new Vector3(Random.Range(1, 4) * 2 - 5, spawnPosition.y, spawnPosition.z);
+                        spawnPosition = new Vector3(Random.Range(1, 5) * 2 - 5, spawnPosition.y, spawnPosition.z);
 
                         int verticalDiffrence = 4 + Mathf.RoundToInt(Random.Range(0, 2 - difficulty / 14) * 4);
                         verticalDiffrence = Mathf.Clamp(verticalDiffrence, 0, 8);
@@ -86,22 +87,24 @@ public class GameManager : MonoBehaviour
                     AddInfoToSpawnArray(type, i + tempI);
                 }
 
-                int verticalDiff = 4 + Mathf.RoundToInt(Random.Range(0 + 3 - difficulty / 5, 12 - difficulty / 5) * 4);
+                int verticalDiff = 4 + Mathf.RoundToInt(Random.Range(1 - difficulty / 2f, 4 - difficulty /1.3f ) * 4);
                 verticalDiff = Mathf.Clamp(verticalDiff, 0, 60);
                 spawnPosition += Vector3.forward * verticalDiff;
 
                 i += segemtnLength - 1;
+
+                shieldSpawnMinPrecentage *= .999f;
             }
             #endregion
 
             #region Shield
             else if (type == SpawnInformation.ObjectType.Shield)
             {
-                spawnPosition = new Vector3(Random.Range(1, 4) * 2 - 5, 1.25f, spawnPosition.z);
+                spawnPosition = new Vector3(Random.Range(1, 5) * 2 - 5, 1.25f, spawnPosition.z);
 
                 AddInfoToSpawnArray(type, i);
 
-                int verticalDiff = 4 + Mathf.RoundToInt(Random.Range(0 + 3 - difficulty / 5, 12 - difficulty / 5) * 4);
+                int verticalDiff = 4 + Mathf.RoundToInt(Random.Range(0, 2) * 4);
                 verticalDiff = Mathf.Clamp(verticalDiff, 0, 60);
                 spawnPosition += Vector3.forward * verticalDiff;
             }
@@ -117,7 +120,7 @@ public class GameManager : MonoBehaviour
         spawnInfo[i].objectType = type;
         spawnInfo[i].position = spawnPosition;
 
-        difficulty = Mathf.RoundToInt(Mathf.Pow(difficultyB, difficultyX++));
+        difficulty = Mathf.RoundToInt(Mathf.Pow(initialValue, initialValuePower++));
     }
 
     void SpawnObject(SpawnInformation info) 
