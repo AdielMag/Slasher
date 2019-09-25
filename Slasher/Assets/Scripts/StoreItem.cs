ad5a72e00b4f997b3b601d2cf0302ad590bd5227
Bocha <Adiel12430@gmail.com>
2019-09-25 18:12:46 +0300

    Store Update
    Ui visualization.
    Buy and equip

---
+++ Slasher/Assets/Scripts/StoreItem.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StoreItem : MonoBehaviour
+{
+    public enum itemType { Character, Weapon }
+    public itemType type;
+
+    public int price;
+    public bool bought;
+    public bool equipped;
+}
--- Slasher/Assets/Scripts/StoreManager.cs
+++ Slasher/Assets/Scripts/StoreManager.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class StoreManager : MonoBehaviour
 {
@@ -10,15 +11,21 @@ public class StoreManager : MonoBehaviour
         instance = this;
     }
 
+    public int coins;
+    [Space]
 
     public Transform myCamera;
     Animator objectDisplayer;
     Quaternion origRot;
 
     public Animator storeAC, mainMenuAc;
+    public Text buyOrEquip;
 
-    bool weapons, characters;
+    Transform charactersParent, weaponsParent;
+    bool weapons = true, characters;
+
     int weaponItemNum, charactersItemNum;
+    int equippedWeaponItemNum = 0, equippedCharacterItemNum = 0;
 
     LoadingScreen loadScr;
     Animator anim;
@@ -29,9 +36,11 @@ public class StoreManager : MonoBehaviour
 
         objectDisplayer = myCamera.GetChild(2).GetComponent<Animator>();
         origRot = myCamera.rotation;
+
+        weaponsParent = objectDisplayer.transform.GetChild(2);
+        charactersParent = objectDisplayer.transform.GetChild(3);
+
     }
-
-
 
     public void OpenStore()
     {
@@ -48,6 +57,7 @@ public class StoreManager : MonoBehaviour
         loadScr.Done();
         loadScr.Enter();
         yield return new WaitForSeconds(.3f);
+        OpenMenu("Weapons");
         myCamera.rotation = Quaternion.Euler(0, -90, 0);
         objectDisplayer.gameObject.SetActive(true);
         storeAC.SetBool("On", true);
@@ -73,12 +83,47 @@ public class StoreManager : MonoBehaviour
 
     public void NextItem()
     {
-        if (weapons) { }
+        if (weapons)
+        {
+            if (weaponItemNum == weaponsParent.childCount -1)
+                return;
+            weaponsParent.GetChild(weaponItemNum).gameObject.SetActive(false);
+            weaponItemNum++;
+            weaponsParent.GetChild(weaponItemNum).gameObject.SetActive(true);
+        }
         else
-        { }
+        {
+            if (charactersItemNum == charactersParent.childCount -1)
+                return;
+            charactersParent.GetChild(charactersItemNum).gameObject.SetActive(false);
+            charactersItemNum++;
+            charactersParent.GetChild(charactersItemNum).gameObject.SetActive(true);
+        }
+
+        CheckItemConditions();
+
     }
     public void LastItem()
     {
+        if (weapons)
+        {
+            if (weaponItemNum == 0)
+                return;
+            weaponsParent.GetChild(weaponItemNum).gameObject.SetActive(false);
+            weaponItemNum--;
+            weaponsParent.GetChild(weaponItemNum).gameObject.SetActive(true);
+        }
+        else
+        {
+            if (charactersItemNum == 0)
+                return;
+            charactersParent.GetChild(charactersItemNum).gameObject.SetActive(false);
+            charactersItemNum--;
+            charactersParent.GetChild(charactersItemNum).gameObject.SetActive(true);
+        }
+
+        CheckItemConditions();
+
     }
 
     public void OpenMenu(string menuType)
@@ -97,6 +142,66 @@ public class StoreManager : MonoBehaviour
             objectDisplayer.SetBool("Weapons", false);
             objectDisplayer.SetBool("Characters", true);
         }
+        CheckItemConditions();
     }
-    public void BuyItem() { }
+
+    void CheckItemConditions( )
+    {
+        StoreItem itemToCheck;
+        if (weapons)
+            itemToCheck = weaponsParent.GetChild(weaponItemNum).GetComponent<StoreItem>();
+        else
+            itemToCheck = charactersParent.GetChild(charactersItemNum).GetComponent<StoreItem>();
+
+        if(itemToCheck.equipped)
+            buyOrEquip.text = "Equipped";
+        else if (itemToCheck.bought)
+            buyOrEquip.text = "Equip";
+        else
+            buyOrEquip.text = "Buy";
+
+    }
+
+    public void BuyItem()
+    {
+        StoreItem currentItem;
+        if (weapons)
+            currentItem = weaponsParent.GetChild(weaponItemNum).GetComponent<StoreItem>();
+        else
+            currentItem = charactersParent.GetChild(charactersItemNum).GetComponent<StoreItem>();
+
+        if (coins >= currentItem.price)
+        {
+            if (currentItem.bought)
+            {
+                currentItem.equipped = true;
+                if (weapons && weaponItemNum != equippedWeaponItemNum)
+                    weaponsParent.GetChild(equippedWeaponItemNum).GetComponent<StoreItem>().equipped = false;
+                else if(characters && charactersItemNum != equippedCharacterItemNum )
+                    charactersParent.GetChild(equippedCharacterItemNum).GetComponent<StoreItem>().equipped = false;
+
+
+                EquipItem();
+            }
+            else
+                currentItem.bought = true;
+        }
+        CheckItemConditions();
+    }
+
+    public Transform playerCharacters, playerWeapons;
+    void EquipItem()
+    {
+        if (weapons)
+            playerWeapons.GetChild(equippedWeaponItemNum).gameObject.SetActive(false);
+        else
+            playerCharacters.GetChild(equippedCharacterItemNum).gameObject.SetActive(false);
+
+        equippedCharacterItemNum = weapons ? weaponItemNum : charactersItemNum;
+
+        if (weapons)
+            playerWeapons.GetChild(equippedWeaponItemNum).gameObject.SetActive(true);
+        else
+            playerCharacters.GetChild(equippedCharacterItemNum).gameObject.SetActive(true);
+    }
 }
